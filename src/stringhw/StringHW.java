7b04c205f2b32df8cb8e247e21e5ab745c843b09
Kml251 <2024455@student.cct.ie>
2024-12-11 12:54:48 +0000

    New project to test git

--- src/stringhw/StringHW.java
+++ src/stringhw/StringHW.java
@@ -23,17 +23,28 @@ public class StringHW {
         InputUtilities myInput = new InputUtilities();
         
         String result = "";// Variable to store the reversed version of the name
-       
+               
         // Using InputUtilities to prompt the user to enter a username
         userName = myInput.askUserForText("Please enter your name: ");
-        
+             
         // To return the user name using a loop
         for(int i = userName.length()-1; i >=0; i--) {
         result +=userName.charAt(i); // Each character should be attached in reverse order.
-        
+                
     }
         // To show output the reversed name to the user
         System.out.println("Your name read backwards is " + result);
+        
+        //To calculate user's name lenght
+        System.out.println("Your name's lenght is: " + result.length());
+        
+        String AllUpper =""; //Variable to store a new reversed version of the name
+        
+        AllUpper = userName.toUpperCase(); //Making user's name all capital
+        
+        //To print user's name with Upper charecters
+        System.out.println("Your name is: " + AllUpper);
+        
         
     }
     
